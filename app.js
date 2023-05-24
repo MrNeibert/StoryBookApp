e6a28250aa4e30a2373f2f4cf22007bc4675c268
Willian <willian.neibertz@gmail.com>
2023-05-24 16:04:19 -0300

    Working google auth login

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require ('express-session')
 const connectDB = require ('./config/db.js')
+
+
 
 //Load config
 dotenv.config({ path: './config/config.env'})
+
+
+// passsport config
+require('./config/passport.js')(passport)
 
 
 connectDB()
@@ -20,11 +28,23 @@ if (process.env.NODE_ENV === 'development'){
 app.engine('.hbs',exphbs.engine({defaultLayout: 'main',extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+//session middleware
+app.use(session({
+  secret: 'keyboard dog',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Static folder
 app.use(express.static(path.join(__dirname,'public')))
 
 //Routes
 app.use('/', require ('./routes/index.js'))
+app.use('/auth', require ('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
