5ee11fa2cb830238a162ff24d6048d3e050b67c4
Willian <willian.neibertz@gmail.com>
2023-05-24 14:56:43 -0300

    Added routes, views and googleAuth

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require ('./config/db.js')
 
 //Load config
 dotenv.config({ path: './config/config.env'})
 
 
+connectDB()
+
 const app = express()
+//logging
+if (process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+//handlebars
+app.engine('.hbs',exphbs.engine({defaultLayout: 'main',extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+//Static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/', require ('./routes/index.js'))
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+
+// @desc   Login/Landing page
+//@route   GET/
+router.get('/',(req,res)=> {
+  res.render('login', {
+    layout: 'login',
+  })
+})
+
+// @desc   DashBoard
+//@route   GET/ dashboard
+
+router.get('/dashboard',(req,res)=> {
+  res.render('dashboard')
+})
+
+module.exports = router
